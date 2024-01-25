1f68e0c06864848960e615bf60eb577ed7d8b135
Loguprasanth G P <loguprasanth.gp@sudoboat.com>
2024-01-25 16:04:59 +0530

    updates

--- react-bp/src/components/upload.js
+++ react-bp/src/components/upload.js
@@ -5,6 +5,10 @@ import RefContext from "Utilities/refContext";
 import axios from "axios";
 import SearchIcon from "@mui/icons-material/Search";
 import FilterListIcon from "@mui/icons-material/FilterList";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { styled } from "@mui/material/styles";
+
 
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
@@ -14,6 +18,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
+
 
 const Upload = () => {
   const [salesProfit,setSalesProfit]=useState("");
@@ -27,7 +32,6 @@ const Upload = () => {
   const { testData } = store;
   useEffect(() => {
     getReloadData();
-    
     
   }, []);
   
@@ -51,7 +55,9 @@ const Upload = () => {
   
 
   const [selectedFile, setSelectedFile] = useState(null);
-  const handleFileChange = (event) => {
+
+
+  const handleFileChange = async (event) => {
     setSelectedFile(event.target.files[0]);
     console.log(event.target.files[0],"file");
   };
@@ -65,53 +71,49 @@ const Upload = () => {
     getAllData(formData);
   };
   // table
+  const assetColumn=[
+    {
+      id:"Companies",
+      label:"Companies"
+    },
+    {
+      id:"Shares ( % )",
+      label:"Shares ( % )"
+    }
+  ]
   const columns = [
-    { id: "name", label: "Name", minWidth: 170 },
-    { id: "code", label: "ISO\u00a0Code", minWidth: 100 },
+    { id: "Product ID", label: "Product ID", minWidth: 170 },
     {
-      id: "population",
-      label: "Population",
+      id: "Product Name",
+      label: "Product Name",
       minWidth: 170,
       align: "right",
       format: (value) => value.toLocaleString("en-US"),
     },
     {
-      id: "size",
-      label: "Size\u00a0(km\u00b2)",
+      id: "Sales Amount",
+      label: "Sales Amount",
       minWidth: 170,
       align: "right",
-      format: (value) => value.toLocaleString("en-US"),
+      format: (value) => value.toFixed(2),
+
     },
     {
-      id: "density",
-      label: "Density",
+      id: "Cost",
+      label: "Cost",
       minWidth: 170,
       align: "right",
       format: (value) => value.toFixed(2),
     },
-  ];
-  function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-  }
+    { id: "P/L", label: "P/L", minWidth: 100 ,
+      format: (value) => value.toFixed(2),
+    },
 
-  const rows = [
-    createData("India", "IN", 1324171354, 3287263),
-    createData("China", "CN", 1403500365, 9596961),
-    createData("Italy", "IT", 60483973, 301340),
-    createData("United States", "US", 327167434, 9833520),
-    createData("Canada", "CA", 37602103, 9984670),
-    createData("Australia", "AU", 25475400, 7692024),
-    createData("Germany", "DE", 83019200, 357578),
-    createData("Ireland", "IE", 4857000, 70273),
-    createData("Mexico", "MX", 126577691, 1972550),
-    createData("Japan", "JP", 126317000, 377973),
-    createData("France", "FR", 67022000, 640679),
-    createData("United Kingdom", "GB", 67545757, 242495),
-    createData("Russia", "RU", 146793744, 17098246),
-    createData("Nigeria", "NG", 200962417, 923768),
-    createData("Brazil", "BR", 210147125, 8515767),
   ];
+  
+  
+  
+
 
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
@@ -131,21 +133,19 @@ const Upload = () => {
   
 
 
-    
-
-
-
-
-
-
+  
 
 
 
   return (
     <>
       <div>
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={handleUpload}>Upload Excel</button>
+        
+        <input type="file"  style={{}} onChange={handleFileChange} />
+        <Button component="label" variant="contained"  onClick={handleUpload} startIcon={<CloudUploadIcon />}>
+      Upload file
+
+        </Button>
       </div>
       <div>
         <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -165,12 +165,12 @@ const Upload = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows
-                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                {(salesProfit || [])?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                   .map((row) => {
                     return (
                       <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
                         {columns.map((column) => {
+                          console.log(row[column.id],"row coloum id",row,column)
                           const value = row[column.id];
                           return (
                             <TableCell key={column.id} align={column.align}>
@@ -187,15 +187,67 @@ const Upload = () => {
             </Table>
           </TableContainer>
           <TablePagination
-            rowsPerPageOptions={[10, 25, 100,5]}
+            rowsPerPageOptions={[10, 25, 100]}
             component="div"
-            count={rows.length}
+            count={salesProfit?.length || 10}
             rowsPerPage={rowsPerPage}
             page={page}
             onPageChange={handleChangePage}
             onRowsPerPageChange={handleChangeRowsPerPage}
           />
         </Paper>
+      
+      </div>
+      <div>
+        <Paper sx={{ width: "100%", overflow: "hidden" ,margin:"50px 0px"}}>
+          <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  {assetColumn.map((column) => (
+                    <TableCell
+                      key={column.id}
+                      align={column.align}
+                      style={{ minWidth: column.minWidth }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {(assets || [])?.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((row) => {
+                    return (
+                      <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                        {assetColumn.map((column) => {
+                          console.log(row[column.id],"row coloum id",row,column)
+                          const value = row[column.id];
+                          return (
+                            <TableCell key={column.id} align={column.align}>
+                              {column.format && typeof value === "number"
+                                ? column.format(value)
+                                : value}
+                            </TableCell>
+                          );
+                        })}
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={assets?.length || 10}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      
       </div>
     </>);
   //enable this if need to use DB json
